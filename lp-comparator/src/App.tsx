6262bbbdbbda9a2f15ceb1845fd3c3fb3b6d68f5
MJ <maraarin@gmail.com>
2022-08-19 14:31:08 +0200

    Added refresh button, sorted dependencies

--- lp-comparator/src/App.tsx
+++ lp-comparator/src/App.tsx
@@ -1,27 +1,42 @@
-import { useTable, useSortBy } from "react-table";
-import styled from "styled-components";
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Button } from "./component/button";
 import { useOfferCalculator } from "./component/OfferCalculator";
 import { offerTableDefinition, Styles, Table } from "./component/OfferTable";
 
 function App() {
-  const { loyaltyOffers, marketData, isLoading, isCalculating } =
-    useOfferCalculator();
+    const {
+        enrichedLoyaltyOffers,
+        marketData,
+        isLoading,
+        isCalculating,
+        setFetchedTimestamp,
+    } = useOfferCalculator();
 
-  const columns = React.useMemo(() => offerTableDefinition, [marketData]);
-  const data = React.useMemo(() => loyaltyOffers, [marketData]);
+    const columns = React.useMemo(() => offerTableDefinition, []);
+    const data = React.useMemo(() => enrichedLoyaltyOffers, [marketData]);
 
-  return (
-    <>
-      {isLoading || isCalculating ? (
-        <div>Loading ...</div>
-      ) : (
-        <Styles>
-          <Table columns={columns} data={data} />
-        </Styles>
-      )}
-    </>
-  );
+    return (
+        <>
+            {isLoading ? (
+                <div>Loading ...</div>
+            ) : isCalculating ? (
+                <div>Calculating ...</div>
+            ) : (
+                <>
+                    <Button
+                        type="button"
+                        value="Refresh table"
+                        onClick={() =>
+                            setFetchedTimestamp(new Date().toString())
+                        }
+                    />
+                    <Styles>
+                        <Table columns={columns} data={data} />
+                    </Styles>
+                </>
+            )}
+        </>
+    );
 }
 
 export default App;
